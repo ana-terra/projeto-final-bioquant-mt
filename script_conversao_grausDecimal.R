6ca73d89da2a38ebcba3acd5a018797b9604eb8c
Marcela Ponce <pls.marcela@gmail.com>
2020-11-24 19:50:59 -0300

    kali uchis stan

--- script_conversao_grausDecimal.R
+++ script_conversao_grausDecimal.R
@@ -1,4 +1,12 @@
 
 #SCRIPT CONVERSAO DE DADOS ESPACIAIS - GRAUS PARA DECIMAIS#
 
+# planilha dados em graus p/ conversão
+library(readxl)
+planilha_coordenadas_graus <- read_excel("C:/Users/User/Downloads/Biologia Quantitativa/Trabalho Final- Material/planilha_coordenadas_graus.xlsx")
+View(planilha_coordenadas_graus)
 
+# planilha dados em decimal p/ comparação
+library(readxl)
+coordenadas_decimal <- read_excel("C:/Users/User/Downloads/Biologia Quantitativa/Trabalho Final- Material/coordenadas_decimal.xlsx")
+View(coordenadas_decimal)
